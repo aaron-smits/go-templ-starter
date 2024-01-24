3e2d9a7ae7dd029ef9ddb4b6da4db66391b7dc73
aaron <aaronsmitstc@gmail.com>
2024-01-23 19:15:45 -0500

    Add Supabase integration to HomeHandler

--- handler/home.go
+++ handler/home.go
@@ -1,13 +1,34 @@
 package handler
 
 import (
+	"os"
+
 	"github.com/aaron-smits/templ-starter/view/homeview"
 	"github.com/labstack/echo/v4"
+
+	supa "github.com/nedpals/supabase-go"
 )
 
 type HomeHandler struct {
 }
 
 func (h HomeHandler) HandleHomeShow(c echo.Context) error {
-	return render(c, homeview.Home())
+
+	supabaseURL := os.Getenv("SUPABASE_URL")
+	supabaseKey := os.Getenv("SUPABASE_KEY")
+	supabase := supa.CreateClient(supabaseURL, supabaseKey)
+	token, err := c.Cookie("access_token")
+	if token == nil {
+		return render(c, homeview.Home(""))
+	}
+	if err != nil {
+		return render(c, homeview.Home(""))
+	}
+	user, err := supabase.Auth.User(c.Request().Context(), token.Value)
+
+	if err != nil {
+		return render(c, homeview.Home(""))
+	}
+
+	return render(c, homeview.Home(user.Email))
 }
