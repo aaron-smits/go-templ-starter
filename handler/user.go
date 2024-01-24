1c62bdccbac3b9ce6d054dda0c601470adbf3f72
aaron <aaronsmitstc@gmail.com>
2024-01-23 20:11:12 -0500

    Add code verifier cookie and update login callback

--- handler/user.go
+++ handler/user.go
@@ -1,33 +1,17 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"time"
 
-	"github.com/aaron-smits/templ-starter/model"
-	"github.com/aaron-smits/templ-starter/view/userview"
 	"github.com/labstack/echo/v4"
 
 	supa "github.com/nedpals/supabase-go"
 )
 
 type UserHandler struct {
-}
-
-type CodeVerifier struct {
-	CodeVerifier string `json:"code_verifier"`
-}
-
-// Create storage for the code verifier to pass it between login and callback
-var codeVerifier CodeVerifier
-
-func (h UserHandler) HandleUserShow(c echo.Context) error {
-	u := model.User{
-		Email: "test@example.com",
-		ID:    1,
-	}
-	return render(c, userview.Show(u))
 }
 
 func (h UserHandler) HandleUserLoginPost(c echo.Context) error {
@@ -45,9 +29,14 @@ func (h UserHandler) HandleUserLoginPost(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	codeVerifier = CodeVerifier{
-		CodeVerifier: ProviderSignInDetails.CodeVerifier,
-	}
+
+	c.SetCookie(&http.Cookie{
+		Name:     "code_verifier",
+		Value:    ProviderSignInDetails.CodeVerifier,
+		HttpOnly: true,
+		Domain:   "localhost",
+		Path:     "/",
+	})
 	return c.Redirect(http.StatusFound, ProviderSignInDetails.URL)
 }
 
@@ -55,25 +44,57 @@ func (h UserHandler) HandleUserLoginCallback(c echo.Context) error {
 	supabaseURL := os.Getenv("SUPABASE_URL")
 	supabaseKey := os.Getenv("SUPABASE_KEY")
 	supabase := supa.CreateClient(supabaseURL, supabaseKey)
+	codeVerifier, err := c.Cookie("code_verifier")
+	if err != nil {
+		return err
+	}
+	if codeVerifier == nil {
+		// return a new error
+		return fmt.Errorf("code verifier cookie not found")
+	}
+
 	ExchangeCodeOptions := supa.ExchangeCodeOpts{
 		AuthCode:     c.QueryParam("code"),
-		CodeVerifier: codeVerifier.CodeVerifier,
+		CodeVerifier: codeVerifier.Value,
 	}
 	ExchangeCodeDetails, err := supabase.Auth.ExchangeCode(c.Request().Context(), ExchangeCodeOptions)
 	if err != nil {
 		return err
 	}
 	c.SetCookie(&http.Cookie{
-		Name:     "access_token",
-		Value:    ExchangeCodeDetails.AccessToken,
+		Name:    "code_verifier",
+		Value:   "",
+		Expires: time.Unix(0, 0),
+		Secure: true,
 		HttpOnly: true,
-		Domain:   "localhost",
-		Path:     "/",
+		SameSite: http.SameSiteStrictMode,
 	})
 	c.SetCookie(&http.Cookie{
-		Name:  "refresh_token",
-		Value: ExchangeCodeDetails.RefreshToken,
+		Name:     "access_token",
+		Value:    ExchangeCodeDetails.AccessToken,
+		Domain:   "localhost",
+		Secure: true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
 	})
+	c.SetCookie(&http.Cookie{
+		Name:   "refresh_token",
+		Value:  ExchangeCodeDetails.RefreshToken,
+		Domain: "localhost",
+		Secure: true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+
+	})
+	c.SetCookie(&http.Cookie{
+		Name: "user_id",
+		Value: ExchangeCodeDetails.User.ID,
+		Domain: "localhost",
+		Secure: true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+	})
+
 	return c.Redirect(http.StatusFound, "/")
 }
 
@@ -92,14 +113,18 @@ func (h UserHandler) HandleUserLogoutPost(c echo.Context) error {
 	c.SetCookie(&http.Cookie{
 		Name:    "access_token",
 		Value:   "",
-		Path:    "/",
 		Expires: time.Unix(0, 0),
+		Secure: true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
 	})
 	c.SetCookie(&http.Cookie{
 		Name:    "refresh_token",
 		Value:   "",
-		Path:    "/",
 		Expires: time.Unix(0, 0),
+		Secure: true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
 	})
 
 	return c.Redirect(http.StatusFound, "/")
