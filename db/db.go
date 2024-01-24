57679fc27e3014c41df559370a0b2ef1ab5b57f6
aaron <aaronsmitstc@gmail.com>
2024-01-23 22:45:56 -0500

    Refactor SupabaseDB code and remove unnecessary fields in user table

--- db/db.go
+++ db/db.go
@@ -50,12 +50,12 @@ func NewPostgresDB() (*PostgresDB, error) {
 	return &PostgresDB{DB: db}, nil
 }
 
-func NewSupabaseDB() (*SupabaseDB, error) {
-	supabaseURL := os.Getenv("SUPABASE_URL")
-	supabaseKey := os.Getenv("SUPABASE_KEY")
-	supabase := supa.CreateClient(supabaseURL, supabaseKey)
-	return &SupabaseDB{DB: supabase}, nil
-}
+// func NewSupabaseDB() (*SupabaseDB, error) {
+// 	supabaseURL := os.Getenv("SUPABASE_URL")
+// 	supabaseKey := os.Getenv("SUPABASE_KEY")
+// 	supabase := supa.CreateClient(supabaseURL, supabaseKey)
+// 	return &SupabaseDB{DB: supabase}, nil
+// }
 
 func (db *PostgresDB) CreateUserTable() error {
 	_, err := db.DB.Exec(`CREATE TABLE IF NOT EXISTS users (
@@ -63,8 +63,6 @@ func (db *PostgresDB) CreateUserTable() error {
 		email VARCHAR(255) NOT NULL,
 		first_name VARCHAR(255) NOT NULL,
 		last_name VARCHAR(255) NOT NULL,
-		encrypted_password VARCHAR(255) NOT NULL,
-		is_admin BOOLEAN NOT NULL,
 		is_archived BOOLEAN NOT NULL,
 		created_at TIMESTAMP NOT NULL,
 		updated_at TIMESTAMP NOT NULL
@@ -76,11 +74,11 @@ func (db *PostgresDB) CreateUserTable() error {
 }
 
 func (db *PostgresDB) CreateUser(u *model.User) (*model.User, error) {
-	query := `INSERT INTO users (email, first_name, last_name, encrypted_password, is_admin, is_archived, created_at, updated_at)
-	VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`
+	query := `INSERT INTO users (email, first_name, last_name, is_archived, created_at, updated_at)
+	VALUES ($1, $2, $3, $4, $5, $6);`
 	row := db.DB.QueryRow(
 		query,
-		u.Email, u.FirstName, u.LastName, u.EncryptedPassword, u.IsAdmin, u.IsArchived, u.CreatedAt, u.UpdatedAt)
+		u.Email, u.FirstName, u.LastName, u.IsArchived, u.CreatedAt, u.UpdatedAt)
 
 	err := row.Scan(&u.ID)
 	if err != nil {
@@ -102,12 +100,12 @@ func (db *PostgresDB) DeleteUser(id int) error {
 func (db *PostgresDB) UpdateUserByID(id int, u *model.User) (*model.User, error) {
 	query := `
 	UPDATE users
-	SET email = $1, first_name = $2, last_name = $3, encrypted_password = $4, is_admin = $5, is_archived = $6, updated_at = $7`
+	SET email = $1, first_name = $2, last_name = $3, is_archived = $4, updated_at = $5`
 	if u == nil {
 		return nil, fmt.Errorf("user cannot be nil")
 	}
 
-	_, err := db.DB.Exec(query, u.Email, u.FirstName, u.LastName, u.EncryptedPassword, u.IsAdmin, u.IsArchived, u.UpdatedAt)
+	_, err := db.DB.Exec(query, u.Email, u.FirstName, u.LastName, u.IsArchived, u.UpdatedAt)
 	if err != nil {
 		return nil, fmt.Errorf("error updating user: %v", err)
 	}
@@ -124,7 +122,7 @@ func (db *PostgresDB) GetUserByID(id int) (*model.User, error) {
 	user := new(model.User)
 	query := `SELECT * FROM users WHERE id = $1;`
 	row := db.DB.QueryRow(query, id)
-	err := row.Scan(&user.ID, &user.Email, &user.FirstName, &user.LastName, &user.EncryptedPassword, &user.IsAdmin, &user.IsArchived, &user.CreatedAt, &user.UpdatedAt)
+	err := row.Scan(&user.ID, &user.Email, &user.FirstName, &user.LastName, &user.IsArchived, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		return nil, fmt.Errorf("error scanning user: %v", err)
 	}
