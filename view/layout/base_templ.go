8b7f1453c7d91f4c3ba8d5be891ae0744fe6c511
aaron <aaronsmitstc@gmail.com>
2024-01-25 15:15:42 -0500

    Remove unnecessary h1 tag from base template

--- view/layout/base_templ.go
+++ view/layout/base_templ.go
@@ -32,16 +32,7 @@ func Base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title></head><body><div id=\"content\"><h1>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Var3 := `Hello world!!!!`
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var3)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</h1><content>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title></head><body><div id=\"content\"><content>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
