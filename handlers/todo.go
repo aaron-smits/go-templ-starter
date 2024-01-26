64ae3cf82bab672e2e72ee2d0504a4c5ada5e163
Aaron <aaronsmitstc@gmail.com>
2024-01-25 19:42:27 -0600

    Refactor todo handler and add delete functionality

--- handlers/todo.go
+++ handlers/todo.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"strconv"
+
 	"github.com/aaron-smits/templ-starter/db"
 	"github.com/aaron-smits/templ-starter/model"
+	"github.com/aaron-smits/templ-starter/view/components"
 	"github.com/aaron-smits/templ-starter/view/homeview"
 	"github.com/labstack/echo/v4"
 )
@@ -13,9 +16,6 @@ type TodoHandler struct {
 func (h TodoHandler) HandleTodoPost(c echo.Context) error {
 	user := c.Get("user").(*model.User)
 	userId := user.User.ID
-	if userId == "" {
-		return Render(c, homeview.LoggedOutHome())
-	}
 	title := c.FormValue("title")
 	if title == "" {
 		return Render(c, homeview.Home(user, db.TodoList))
@@ -32,7 +32,7 @@ func (h TodoHandler) HandleTodoPost(c echo.Context) error {
 
 	// append the new todo to the list
 	db.TodoList = append(db.TodoList, todo)
-	return Render(c, homeview.Home(user, db.TodoList))
+	return Render(c, components.TodoList(db.TodoList))
 }
 
 func (h TodoHandler) HandleTodoPut(c echo.Context) error {
@@ -47,16 +47,21 @@ func (h TodoHandler) HandleTodoPut(c echo.Context) error {
 			db.TodoList[i] = *todo
 		}
 	}
-	return c.JSON(200, db.TodoList)
+	return Render(c, components.TodoList(db.TodoList))
 }
 
-// func (h TodoHandler) HandleTodoDelete(c echo.Context) error {
-// 	// Iterate through the list and remove the todo with the matching ID
-// 	for i, t := range db.TodoList {
-// 		if t.ID == c.Param("id") {
-// 			db.TodoList = append(db.TodoList[:i], db.TodoList[i+1:]...)
-// 		}
-// 	}
-
-// 	return c.JSON(200, db.TodoList)
-// }
+func (h TodoHandler) HandleTodoDelete(c echo.Context) error {
+	id := c.Param("id")
+	// convert id to int
+	strId, err := strconv.Atoi(id)
+	if err != nil {
+		return err
+	}
+	// Find the todo in the list with the matching ID
+	for i, t := range db.TodoList {
+		if t.ID == strId {
+			db.TodoList = append(db.TodoList[:i], db.TodoList[i+1:]...)
+		}
+	}
+	return Render(c, components.TodoList(db.TodoList))
+}
